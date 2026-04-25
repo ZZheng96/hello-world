8344bc65c205198d1e13b3905f653dc9fd9be3f7
ZZheng96 <karlozone@163.com>
2026-04-26 06:50:27 +0800

    create poj

--- hello-C/learnC.cpp
+++ hello-C/learnC.cpp
@@ -1,5 +1,10 @@
 #include <stdio.h>
 int main(){
     puts("hello world");
+    printf("%03d\n",1+2);
+    printf("%.2f\n",8/5);
+    // int a,b;
+    // scanf("%d%d", &a, &b);
+    // printf("%d\n%d",a,b);
     return 0;
 }
