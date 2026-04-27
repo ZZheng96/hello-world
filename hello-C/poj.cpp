6925121c291d83cb9437aee2b5e8a3b9d0d0ea73
ZZheng96 <karlozone@163.com>
2026-04-27 20:22:06 +0800

    427

--- hello-C/poj.cpp
+++ hello-C/poj.cpp
@@ -1,14 +1,13 @@
-#include <stdio.h>
-int main()
-{
-int n1 = 15;
-short n2 = -15;
-unsigned short n3 = 0xffe0;
-char c = 15;
-n1 = n1>>2;
-n2 >>= 3;
-n3 >>= 4;
-c >>= 3;
-printf( "n1=%d,n2=%x,n3=%x,c=%x",n1,n2,n3,c);
-} //输出结果是：n1=3,n2=fffffffe,n3=ffe,c=1
-// n2被自动转换成int输出
+#include <iostream>
+using namespace std;
+int main(){
+    unsigned int x;
+int y;
+    cin >> x>>y;
+    if( x>y ) 
+        cout << ">" ;
+    else if(x==y)
+        cout << "=";
+else cout << "<";
+    return 0;
+}
