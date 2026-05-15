5527ce93dcf81bd74d2b6f4b94ac9fd478a0de37
ZZheng96 <karlozone@163.com>
2026-05-15 18:49:26 +0800

    0515

--- hello-C/poj.cpp
+++ hello-C/poj.cpp
@@ -1,11 +1,36 @@
-#include <iostream>
-using namespace std;
-int main(){
+// #include <iostream>
+// using namespace std;
+// int main(){
 
-    int a;
-    cin >> a;
-    if(a/3.0+50>a/1.2) cout << "Walk";
-    else if(a/3.0+50==a/1.2) cout << "All";
-    else cout << "Bike";
-    return 0;
+//     int a;
+//     cin >> a;
+//     if(a/3.0+50>a/1.2) cout << "Walk";
+//     else if(a/3.0+50==a/1.2) cout << "All";
+//     else cout << "Bike";
+//     return 0;
+// }
+
+#include <stdio.h>
+#include <string.h>
+ 
+int main ()
+{
+   char str1[14] = "xiaozhi";
+   char str2[14] = "google";
+   char str3[14];
+   int  len ;
+ 
+   /* 复制 str1 到 str3 */
+   strcpy(str3, str1);
+   printf("strcpy( str3, str1) :  %s\n", str3 );
+ 
+   /* 连接 str1 和 str2 */
+   strcat( str1, str2);
+   printf("strcat( str1, str2):   %s\n", str1 );
+ 
+   /* 连接后，str1 的总长度 */
+   len = strlen(str1);
+   printf("strlen(str1) :  %d\n", len );
+ 
+   return 0;
 }
